fd83ca74f37b3b19fdee5b05a2020bfab125af1e
Lubava7 <joheunsaram@mail.ru>
2023-02-06 14:19:10 +0400

    Updates

--- src/components/Footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import footer from "./Footer.module.css";
-
-const Footer = () => {
-  return <div className={footer.footer}> © TEST, 1022–2022</div>;
-};
-
-export default Footer;
